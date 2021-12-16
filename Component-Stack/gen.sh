534f1efdfca7bf5248b973bb3088cdc7f63e31eb
6rishka <greg@charyszczak.dev>
2021-12-16 19:34:38 +0100

    Update gen.sh

--- Component-Stack/gen.sh
+++ Component-Stack/gen.sh
@@ -1,7 +1,4 @@
 mkdir $1
 cd Base ; tar -c . | tar -C ../$1 -x
 cd ../$1
-find . -type f -name 'Base*' | while read FILE ; do
-    newfile="$(echo ${FILE} |sed -e 's/\Base/${1}/')" ;
-    mv "${FILE}" "${newfile}" ;
-done
+find -name "*Base*.swift" -exec rename "s/Base/$1/" {} ";"
